0fd325b5b47094300e6a84fcbcf50b179fd3312c
Suence <Suence.Online@outlook.com>
2025-06-19 17:37:56 +0800

    添加安装程序项目

---
+++ SRC/Installer/PowerLab.Installer.Bootstrapper/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using PowerLab.Installer.Bootstrapper.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace PowerLab.Installer.Bootstrapper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+
+}
