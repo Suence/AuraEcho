0121b5f77b3f89753e1390ae8acbcce02f3683a3
Suence <Suence.Online@outlook.com>
2025-12-15 16:04:05 +0800

    :loud_sound: 为 PluginInstallService 类增加日志

--- SRC/Shared/PowerLab.Core/Services/PluginInstallService.cs
+++ SRC/Shared/PowerLab.Core/Services/PluginInstallService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.IO.Compression;
 using System.Reflection;
 using System.Text.Json;
@@ -41,7 +42,10 @@ namespace PowerLab.Core.Services
             // 读取并解析 manifest 文件
             string manifestPath = Path.Combine(extractPath, MANIFEST_FILE_NAME);
             if (!File.Exists(manifestPath))
-                throw new FileNotFoundException("插件缺少 manifest 文件。");
+            {
+                _logger.Error("插件缺少 manifest 文件。");
+                return null;
+            }
 
             string manifestJson = File.ReadAllText(manifestPath);
             var manifest = JsonSerializer.Deserialize<PluginManifest>(manifestJson);
@@ -52,13 +56,15 @@ namespace PowerLab.Core.Services
                 Directory.Delete(finalPath, true);
 
             DirectoryUtils.SafeMoveDirectory(extractPath, finalPath);
-
+            _logger.Error("查询已安装信息");
             var existingPlugin = _localPluginRepository.GetPluginRegistries().FirstOrDefault(pr => pr.Manifest.Id == manifest.Id);
             if (existingPlugin is not null)
             {
+                _logger.Error("正在移除已安装信息");
                 _localPluginRepository?.RemovePluginRegistry(existingPlugin.Id);
             }
 
+            _logger.Error("加载程序集");
             var entryAssemblyPath = Path.Combine(finalPath, manifest.EntryAssemblyName);
             var alc = new PluginLoadContext(entryAssemblyPath);
             Assembly pluginAssembly = null;
@@ -71,6 +77,7 @@ namespace PowerLab.Core.Services
                 _logger.Error($"加载插件程序集失败：{manifest.PluginName}，异常：{ex.Message}");
                 return null;
             }
+            _logger.Error("执行插件环境初始化");
             IPluginSetup pluginDatabaseInitializer = GetPluginDatabaseInitializer(pluginAssembly);
             pluginDatabaseInitializer?.Setup(_containerProvider);
             var pluginRegistry = new PluginRegistry
@@ -81,6 +88,7 @@ namespace PowerLab.Core.Services
                 PluginFolder = ApplicationPaths.GetPluginPath(manifest.Id),
             };
             _localPluginRepository.AddPluginRegistry(pluginRegistry);
+            _logger.Error("安装成功");
             return pluginRegistry;
 
             IPluginSetup GetPluginDatabaseInitializer(Assembly pluginAssembly)
